713dc883903f14cba04ab439bf81a9db2c0f89b6
Tyler Bettilyon <teb@tebs-lab.com>
2024-08-30 10:33:51 -0600

    improved the challenge a little

--- 2024-09-01/08_challenge.py
+++ 2024-09-01/08_challenge.py
@@ -64,15 +64,39 @@ def add(a, b):
 
 
 add(1,2)
+add(1,2)
 
 
 '''
 Logs and prints:
 
-INFO:root:<function add at 0x107540c10> called with args: (1, 2) and kwargs: {}
-INFO:root:<function add at 0x107540c10> returned 3
-INFO:root:<function add at 0x107540c10> called with args: (1, 2) and kwargs: {}
-INFO:root:<function add at 0x107540c10> returned 3
+INFO:root:<function add at 0x1032f4c10> called with args: (1, 2) and kwargs: {}
+INFO:root:<function add at 0x1032f4c10> returned 3
+INFO:root:<function add at 0x1032f4c10> called with args: (1, 2) and kwargs: {}
+INFO:root:<function add at 0x1032f4c10> returned 3
 
 MISS, adding ((1, 2), ()) -> (3, 3)
+HIT ((1, 2), ()) -> (3, 3)
+
+Commentary:
+
+Since @log decorates the initial function, everything that 
+prints via the logger is specific to the raw add function. 
+
+@repeat decorates the logged version of add, so when we call
+it the first time we see 2 identical sets of log entries.
+
+@size_limited_cache decorates the repeated-and-logged function.
+This explains two things: 
+
+1) The cached return value is (3, 3) because @repeat returns a 
+   tuple with both return values.
+
+2) The second invocation of the function doesn't log anything.
+   The cache "hits" on the arguments, so the repeated-and-logged 
+   version of the function is not ever called -- we just return
+   the saved return value.
+
+Hopefully this demonstrates the importance of decorator order when 
+you choose to use more than one at a time.
 '''
